faa1f10b9501c5d0a17b35725bb9fc84b8d580e3
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-07-06 19:13:05 -0500

    fin

--- static/js/character.js
+++ static/js/character.js
@@ -8,6 +8,7 @@ export default class Character {
         this.$characterImageContainer = document.querySelector('#character-image-container');
         this.$characterDescriptionContainer = document.querySelector('#character-description-container');
         this.$characterNameContainer = document.querySelector('#character-name-container');
+        this.$characterNamePlaceHolderContainer = document.querySelector('#character-name-placeholder');
         this.render();
     }
 
@@ -39,5 +40,6 @@ export default class Character {
         this.$characterImageContainer.innerHTML = this.buildImage();
         this.$characterNameContainer.innerHTML = this.buildName();
         this.$characterDescriptionContainer.innerHTML = this.buildDescription(); 
+        this.$characterNamePlaceHolderContainer.innerHTML = this.name;
     }
 }
